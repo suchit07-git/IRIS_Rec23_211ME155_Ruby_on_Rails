33ac6fffae9fbade5e807492d609a001b43809e2
Suchit <suchitkarunakaran@gmail.com>
2024-01-12 22:54:11 +0530

    Updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { admin: 'admin', student: 'student', faculty: 'faculty'}
+
+  def admin?
+    role == 'admin'
+  end
+  def student?
+    role == 'student'
+  end
+  def faculty?
+    role == 'faculty'
+  end
 end
